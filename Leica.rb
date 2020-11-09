b1b142fea2b87cb87bb423828f40f536d4486323
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-11-10 01:52:17 +0900

    first commit

---
+++ Leica.rb
@@ -0,0 +1,52 @@
+# 応答のクラス。
+class Resoponder
+
+    def initialize(name)
+     @name = name
+    end
+ 
+    def resoponse(input)
+     return "What is #{input}?"
+    end
+ 
+    def name
+     return @name
+    end
+ end
+ # ライカ。
+ class Leica
+     def initialize(name)
+         @name = name
+         @resoponder = Resoponder.new('What')
+     end
+ 
+     def dialogue(input)
+         return @resoponder.resoponse(input)
+     end
+ 
+     def resoponder_name
+         return @resoponder.name
+     end
+ 
+     def name
+         return @name
+     end
+ end
+ 
+ def prompt(leica)
+     return leica.name + ":" + leica.resoponder_name + ">"
+ end
+ 
+ 
+ 
+ puts("Leica System prototype : proto")
+ proto = Leica.new("proto")
+ while true
+     print(">")
+     input = gets
+     input.chomp!
+     break if input == ""
+ 
+     response = proto.dialogue(input)
+     puts(prompt(proto) + response)
+ end
