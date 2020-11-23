08faf67570ddfcdcdac4f16d6f69aea982b6ddfd
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-11-23 20:49:43 +0900

    Update Leica2.2.rb

--- Leica2.2.rb
+++ Leica2.2.rb
@@ -1,3 +1,16 @@
+require "responder"
+
+class Leica
+    def initialize(name)
+        @name = name 
+        @responder = RandomResponder.new("Random")
+    end
+
+    def dialogue(input)
+        return @responder.response(input)
+    end
+
+    def responder_name
         return @responder.name
     end
 
