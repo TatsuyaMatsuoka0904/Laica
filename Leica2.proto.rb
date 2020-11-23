ee1615d62b61536a93933b5771e7933246b84d46
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-11-23 20:59:52 +0900

    Create Leica2.proto.rb

---
+++ Leica2.proto.rb
@@ -0,0 +1,17 @@
+require 'Leica2'
+
+def prompt(leica)
+    retrun leica.name + ":" + leica.responder_name + ">"
+end
+
+puts ("Leica System prototype : proto")
+proto = Leica.new('proto')
+while true
+    print(">")
+    input = gets
+    input.chomp!
+    break if input == ""
+
+    response = proto.dialogue(input)
+    puts (prompt(proto) + response)
+end
