35ccc7c3089c4f2ce6bbf83bae08396a94db98e3
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-11-29 03:44:45 +0900

    Update Leica.rb

--- Leica.rb
+++ Leica.rb
@@ -4,11 +4,11 @@ class Resoponder
     def initialize(name)
      @name = name
     end
- 
+
     def resoponse(input)
      return "What is #{input}?"
     end
- 
+
     def name
      return @name
     end
@@ -19,20 +19,20 @@ class Resoponder
          @name = name
          @resoponder = Resoponder.new('What')
      end
- 
+
      def dialogue(input)
          return @resoponder.resoponse(input)
      end
- 
+
      def resoponder_name
          return @resoponder.name
      end
- 
+
      def name
          return @name
      end
  end
- 
+
  def prompt(leica)
      return leica.name + ":" + leica.resoponder_name + ">"
  end
